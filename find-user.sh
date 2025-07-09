2303ca3ed9e02348043a42387e4bfa8cd647d4c3
Nazar Yaremiiy <147419137+nyaremiiy@users.noreply.github.com>
2025-07-09 22:51:28 +0300

    Update find-user.sh

--- find-user.sh
+++ find-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Create a bash script to find all users with /bin/bash CLI in a file password - "
 echo "--------------------"
-grep bin/bash /ect/passwd
+grep bin/bash /etc/passwd
 echo "--------------------"
